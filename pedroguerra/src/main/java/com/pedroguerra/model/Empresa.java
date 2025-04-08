0f04d5f0286d331a90d76b12e79efd9da307d579
Fcc2187 <fcc3@cesar.school>
2025-04-08 09:16:47 -0300

    2 classes criadas

---
+++ pedroguerra/src/main/java/com/pedroguerra/model/Empresa.java
@@ -0,0 +1,55 @@
+package main.java.com.pedroguerra.model;
+
+public class Empresa {
+    private String cnpj;
+    private String nome;
+    private String contato;
+    private String numero;
+    private String bairro;
+    private String cidade;
+    private String rua;
+
+    public String getCnpj() {
+        return cnpj;
+    }
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getContato() {
+        return contato;
+    }
+    public void setContato(String contato) {
+        this.contato = contato;
+    }
+    public String getNumero() {
+        return numero;
+    }
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+    public String getBairro() {
+        return bairro;
+    }
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+    public String getCidade() {
+        return cidade;
+    }
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+    public String getRua() {
+        return rua;
+    }
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+
+}
---
+++ pedroguerra/src/main/java/com/pedroguerra/model/Funcionario.java
@@ -0,0 +1,57 @@
+package main.java.com.pedroguerra.model;
+
+public class Funcionario{
+    private String matricula;
+    private String nome;
+    private String contato;
+    private String cidade;
+    private String numero;
+    private String bairro;
+    
+    public String getMatricula() {
+        return matricula;
+    }
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getContato() {
+        return contato;
+    }
+    public void setContato(String contato) {
+        this.contato = contato;
+    }
+    public String getCidade() {
+        return cidade;
+    }
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+    public String getNumero() {
+        return numero;
+    }
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+    public String getBairro() {
+        return bairro;
+    }
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+    public String getRua() {
+        return rua;
+    }
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+    private String rua;
+
+    
+
+}
