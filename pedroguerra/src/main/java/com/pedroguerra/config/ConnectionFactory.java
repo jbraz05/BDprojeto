65d1bdbcfc4763fe00f181e05d17d448c0c39f92
Fcc2187 <fcc3@cesar.school>
2025-04-12 11:30:42 -0300

    melhorei a parte pra n ficar trocando senha(ensino dps pra vcs

--- pedroguerra/src/main/java/com/pedroguerra/config/ConnectionFactory.java
+++ pedroguerra/src/main/java/com/pedroguerra/config/ConnectionFactory.java
@@ -3,19 +3,33 @@ package com.pedroguerra.config;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
+import java.io.InputStream;
 
 public class ConnectionFactory {
 
-    private static final String URL = "jdbc:mysql://localhost:3306/pedroguerra";
-    private static final String USER = "root";
-    private static final String PASSWORD = "Felipe2006$";
+    private static final String CONFIG_FILE = "/config.properties";
 
     public static Connection getConnection() throws SQLException {
-        try { 
+        try (InputStream input = ConnectionFactory.class.getResourceAsStream(CONFIG_FILE)) {
+            if (input == null) {
+                throw new SQLException("Arquivo config.properties não encontrado.");
+            }
+
+            Properties props = new Properties();
+            props.load(input);
+
+            String url = props.getProperty("db.url");
+            String user = props.getProperty("db.user");
+            String password = props.getProperty("db.password");
+
             Class.forName("com.mysql.cj.jdbc.Driver");
-            return DriverManager.getConnection(URL, USER, PASSWORD);
+            return DriverManager.getConnection(url, user, password);
+
         } catch (ClassNotFoundException e) {
             throw new SQLException("Driver JDBC não encontrado!", e);
+        } catch (Exception e) {
+            throw new SQLException("Erro ao carregar config.properties.", e);
         }
     }
 }
