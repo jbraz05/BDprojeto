cc958add3de7d6c2492699cc05c3aa4e170be363
Miguel <miguelbecker@rayconsulting.com.br>
2025-05-29 09:58:38 -0300

    trigger de incrementação de capital adicionado novamente, gabi deu 5 minutos para fazer isso e permitiu o commit

--- pedroguerra/Scripts/triggers.sql
+++ pedroguerra/Scripts/triggers.sql
@@ -62,3 +62,33 @@ BEGIN
     INSERT INTO LogCliente (nome_cliente, cnpj_cpf_cliente, data_insercao)
     VALUES (NEW.nome, NEW.cnpj_cpf, NOW());
 END;
+
+
+ -- Trigger para incrementar o capital social da empresa após a medição de um serviço --
+
+DROP TRIGGER IF EXISTS trg_incrementa_capital_empresa;
+DELIMITER //
+
+CREATE TRIGGER trg_incrementa_capital_empresa
+AFTER UPDATE ON Servico
+FOR EACH ROW
+BEGIN
+    DECLARE cnpj_empresa VARCHAR(14);
+
+    IF NEW.feito = TRUE AND OLD.feito = FALSE THEN
+        SELECT fk_empresa_cnpj
+        INTO cnpj_empresa
+        FROM Possui
+        WHERE fk_servico_id = NEW.id
+        LIMIT 1;
+
+        IF cnpj_empresa IS NOT NULL AND NEW.valor_medicao IS NOT NULL THEN
+            UPDATE Empresa
+            SET capital_social = capital_social + NEW.valor_medicao
+            WHERE cnpj = cnpj_empresa;
+        END IF;
+    END IF;
+END;
+//
+
+DELIMITER ;
